2da27201c80e188dc655565dc2397498bcbd15ac
yrwq <yrwqid@gmail.com>
2024-02-02 09:46:52 +0100

    feature(gitignore): add deploy script to gitignore

--- src/app/music/[slug]/page.tsx
+++ src/app/music/[slug]/page.tsx
@@ -3,34 +3,40 @@ import { PageTitle } from '@/components/PageTitle'
 
 import { music } from "@/lib/music"
 
+import { play, createAudioPlayer } from "play-dl"
+import SoundCloud from "soundcloud-scraper"
+const client = new SoundCloud.Client();
+
 export async function generateStaticParams() {
   return music.map((m) => ({
     slug: m.title.replace(/\s/g, "-")
   }))
 }
 
+function download(url, options = {}) {
+    return new Promise((resolve, reject) => {
+        if (!url || typeof url !== "string") return reject(new Error(`Expected url, received "${typeof url}"!`));
+        const request = url.startsWith("http://") ? require("http") : require("https");
+        request.get(url, options, res => resolve(res));
+    });
+}
+
 export default async function CollectionPage({ params }) {
   const unslug = params.slug.replace(/-/g, " ")
+  const playlist = client.getPlaylist("https://soundcloud.com/prodexa/sets/" + params.slug)
+
   return (
-    <>
-    {music.map((m, idx) => {
-      if (m.title == unslug) {
+    playlist.then(async x => {
+      return (
+        x.tracks.map(function (tr, idx) {
+        var url = "https://soundcloud.com/prodexa/" + tr.title
         return (
-          <div key={idx}>
-            <PageTitle title={m.title}/>
-
-            {m.tracks[0].map((t, idx) => {
-              return (
-                <div key={idx}>
-                  {t.title}
-                </div>
-              )
-            })}
-
+          <div key={idx} className="flex">
+            {play.stream(url)}
           </div>
         )
-      }
-    })}
-    </>
+      })
+      )
+    })
   )
 }
--- src/components/MusicItem.tsx
+++ src/components/MusicItem.tsx
@@ -1,7 +1,12 @@
-export const BookmarkCard = ({ music }) => {
+"use client"
+import Balancer from 'react-wrap-balancer'
+
+import { cn } from '@/lib/utils'
+
+export const MusicItem = ({ title, cover_url, duration, play_count, stream_url, ...rest }) => {
   return (
-    <div>
-      
+    <div className={cn('text-xl', className)}>
+      {title}
     </div>
   )
 }
