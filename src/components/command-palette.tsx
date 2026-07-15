d45bfb6144d013eab4059553f10f8dfe6878f30e
yrwq <yrwqid@gmail.com>
2026-07-15 13:43:39 +0200

    chore: cleanup

--- src/components/command-palette.tsx
+++ src/components/command-palette.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import {
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 import { Terminal } from "lucide-react";
 import * as React from "react";
